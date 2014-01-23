ff0caf87c2327ca3414ebcf9541d4a3de4e6c6ee
Charlie <charlie.hoey@gmail.com>
2014-01-23 09:09:52 -0500

    initial commit, added demo files for Valiant

---
+++ Valiant360/src/main.js
@@ -0,0 +1,31 @@
+require.config({
+	paths: {
+		"jquery": "lib/jquery-1.7.2.min",
+		"threejs": "lib/Three",
+		"detector": "lib/Detector",
+		"valiant": "valiant.jquery",
+		"modernizr": "lib/modernizr-latest"
+	},
+	shim: {
+		"threejs": {
+			exports: "THREE"
+		},
+		"detector": {
+			deps: ['threejs']
+		}
+	}
+});
+
+require([
+	"jquery",
+	"valiant"
+], function($) {
+
+	$(document).ready(function() {
+
+		$(".Valiant360").Valiant360({ debug: true });
+		$(".Valiant360").Valiant360('play');
+	
+	});
+
+});
